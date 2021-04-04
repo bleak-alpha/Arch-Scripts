155e8d8ecff706d01597204a35815b67e0c8e360
bleak-alpha <bleak-alpha@protonmail.ch>
2021-04-04 20:03:31 +0530

    master

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/sh
+
+base_install()
+{	echo "Will Install following packages:
+		1. base and base-developer packages.
+		2. linux kernel, linux kernel headers and linux firmware.
+		3. vim and neovim text editor"	
+	pacstrap -i /mnt base base-devel linux linux-firmware linux-headers vim nvim
+}
+
+while true
+do
+ read -r -p "Are You Sure? [Y/n] " ch
+ 
+ case $ch in
+     [yY][eE][sS]|[yY])
+ 												base_install
+ 												break;;
+     
+		 [nN][oO]|[nN])
+        								echo "Enter a valid choice"
+												continue;;
+     *)
+ 			echo "Invalid input...";;
+ esac
+done
+
+echo "Script will change the root directory now"
+
+arch-chroot /mnt
