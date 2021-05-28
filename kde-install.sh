8815aeb4a8a754b1aa693fdf494679d445e62256
Sankalp A. Thorat <bleak-alpha@protonmail.ch>
2021-05-28 18:25:47 +0530

    Update kde-install.sh

--- kde-install.sh
+++ kde-install.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 printf "Installing X-Org"
-pacman -S xorg xorg-xinit xorg-xsetroot arandr firefox git wget curl wget neovim vim ttf-fira-code ttf-font-awesome otf-fontawesome powerline-fonts awesome-terminal-fonts
+pacman -Syy --noconfirm --asdeps xorg xorg-sever xorg-xinit xorg-xsetroot arandr firefox git wget curl wget neovim vim ttf-fira-code ttf-font-awesome otf-fontawesome powerline-fonts awesome-terminal-fonts
 
 printf "Installing KDE and KDE-Applications"
-pacman -S --noconfirm kde-applications plasma
+pacman -S --noconfirm --asdeps kde-applications plasma
 
 printf "Setting Up xinitrc"
 if [ -e .xinitrc ]
 then
-    if [ "$(sed -n '51p' "$HOME"/.xinitrc)" = "twm &" ] 
+    if [ "$(sed -n '51p' $HOME/.xinitrc)" = "twm &" ] 
     then
-        sed '51,56d' "$HOME"/.xinitrc
+        sudo sed '51,56d' $HOME/.xinitrc
     fi
 else
-    cp /etc/X11/xinit/xinitrc "$HOME"/.xinitrc
-    sed '51,56d' "$HOME"/.xinitrc
+    sudo cp -r /etc/X11/xinit/xinitrc "$HOME"/.xinitrc
+    sudo sed '51,56d' "$HOME"/.xinitrc
 fi
 echo 
 ":'KDE xinit'
