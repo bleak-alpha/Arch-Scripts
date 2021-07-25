00956f1d8b8608e5c67e0631dd69b2b42cb31920
bleak-alpha <bleak-alpha@protonmail.ch>
2021-07-25 21:43:12 +0530

    main

--- aur-packages.sh
+++ aur-packages.sh
@@ -7,4 +7,5 @@ makepkg -si PKGBUILD;
 
 printf "Installing AUR Packages...."
 curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | gpg --import -
-paru -Syy --noconfirm birdtray pnmixer brave-bin pulseaudio-modules-bt sublime-text-3 spotify spotifywm-git protonvpn-cli-ng betterlockscreen 
+paru -Syy --noconfirm birdtray pnmixer brave-bin pulseaudio-modules-bt sublime-text-3 spotify spotifywm-git protonvpn-cli-ng onlyoffice-bin coreinfo betterlockscreen 
+
