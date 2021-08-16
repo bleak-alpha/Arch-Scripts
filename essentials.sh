0d5dbdb7284930b2d312ea43417d542dafed8d37
bleak-alpha <bleak-alpha@protonmail.ch>
2021-08-16 14:57:44 +0530

    main

---
+++ essentials.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#adding pacman repositories 
+sed -i 's/#[multilib]/[multilib]/g' /etc/pacman.conf
+sed -i 's+#Include = /etc/pacman.d/mirrorlist+Include = /etc/pacman.d/mirrorlist+g' /etc/pacman.conf
+
+pacman -Syy --noconfirm --asdeps wine code gnome-keyring python jre-openjdk rust golang ghc cabal-install stack mpv vlc libreoffice-fresh;
+
