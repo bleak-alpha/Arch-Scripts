ac501565a4607087c9fa36b52d6d3e5a305a1db9
bleak-alpha <bleak-alpha@protonmail.ch>
2021-08-16 14:45:43 +0530

    main

--- post-chroot-base-install.sh
+++ post-chroot-base-install.sh
@@ -24,15 +24,15 @@ cp /etc/sudoers /etc/sudoers.bkp; #Backing up sudoers file in case I fuck up
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
 
 printf "Installing Neccessary Packages...."
-pacman -S --noconfirm rsync iptables eptables gufw grub efibootmgr networkmanager iwd dhcpcd mtools dosfstools xdg-user-dirs xdg-utils inetutils dnsutils bluez bluez-utils alsa-utils pulseaudio openssh os-prober xbindkeys acpi acpid p7zip udisks2 
+pacman -S --noconfirm rsync iptables eptables gufw grub efibootmgr networkmanager iwd dhcpcd mtools dosfstools bluez bluez-utils alsa-utils pulseaudio openssh os-prober xbindkeys p7zip udisks2 zathura zathura-cb zathura-djvu zathura-pdf-mupdf zathura-ps xfce4-terminal; #acpi acpid xdg-users-dirs xdg-utils inetutils dnsutils
 
 printf "Enabling Neccessary Services"
 systemctl enable NetworkManager
 systemctl enable dhcpd
 systemctl enable bluetooth
 systemctl enable sshd
-systemctl enable firewalld
-systemctl enable acpid
+#systemctl enable firewalld
+#systemctl enable acpid
 systemctl enable iwd
 
 printf "Installing Grub And Generating Config File...."
